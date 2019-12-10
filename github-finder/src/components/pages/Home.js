c0bd49af41955c4390c6e6276f333d459dae789a
Vaibhav Vinayak <vinayakvaibhav67@gmail.com>
2019-12-10 11:02:59 +0530

    finishing context and hooks and adding netlify config

---
+++ github-finder/src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Search from "../users/Search";
+import Users from "../users/Users";
+
+const Home = () => (
+  <div>
+    <Fragment>
+      <Search />
+      <Users />
+    </Fragment>
+  </div>
+);
+export default Home;
