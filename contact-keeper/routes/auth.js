40d23ff180b7e2cdce3fe47f6a357348479d4903
VaibhavVinayak <= vinayakvaibhav67@gmail.com>
2019-12-17 20:19:58 +0530

    final change in backend

--- contact-keeper/routes/auth.js
+++ contact-keeper/routes/auth.js
@@ -8,13 +8,14 @@ const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
-// protected route
+// protected route using middleware
 router.get("/", auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server error!");
+    res.status(500).send("Server Error!");
   }
 });
 
