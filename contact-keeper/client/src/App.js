287d7d4e09003c4132df8b7dd2984b3f177ee5ed
Vaibhav Vinayak <vinayakvaibhav67@gmail.com>
2019-12-18 12:18:03 +0530

    changes in client and adding contacts context - sync with git

--- contact-keeper/client/src/App.js
+++ contact-keeper/client/src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
-function App() {
-  return <div className="App">App</div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </div>
+      </Fragment>
+    </Router>
+  );
+};
 
 export default App;
---
+++ contact-keeper/client/src/context/contact/ContactState.js
@@ -0,0 +1,4 @@
+import { useReducer } from "react";
+import uuid from "uuid";
+import ContactContact from "./contactContext";
+import ContactReducer from "./contactReducer";
